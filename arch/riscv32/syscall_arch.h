5f9ecd78f4ef873111f0fcc52487f8d3e0e52424
petergu <guyimin@mail.ustc.edu.cn>
2021-08-28 22:13:29 +0800

    syscall modification

--- arch/riscv32/syscall_arch.h
+++ arch/riscv32/syscall_arch.h
@@ -5,21 +5,32 @@
 
 #define __asm_syscall(...) \
 	__asm__ __volatile__ ("scall\n\t" \
-	: "+r"(a0) : __VA_ARGS__ : "memory"); \
-	return a0; \
+	: "+r"(a0) : __VA_ARGS__ : "memory");
+
+// use register a6 for SEND/RECV
+#define PSEUDOS_SEND = 74;
+#define PSEUDOS_RECV = 75;
 
 static inline long __syscall0(long n)
 {
 	register long a7 __asm__("a7") = n;
 	register long a0 __asm__("a0");
-	__asm_syscall("r"(a7))
+	register long a6 __asm__("a6") = PSEUDOS_SEND;
+	__asm_syscall("r"(a7), "r"(a6))
+	a6 = PSEUDOS_RECV;
+	__asm_syscall("r"(a6))
+	return a0;
 }
 
 static inline long __syscall1(long n, long a)
 {
 	register long a7 __asm__("a7") = n;
 	register long a0 __asm__("a0") = a;
-	__asm_syscall("r"(a7), "0"(a0))
+	register long a6 __asm__("a6") = PSEUDOS_SEND;
+	__asm_syscall("r"(a7), "0"(a0), "r"(a6))
+	a6 = PSEUDOS_RECV;
+	__asm_syscall("r"(a6))
+	return a0;
 }
 
 static inline long __syscall2(long n, long a, long b)
@@ -27,7 +38,11 @@ static inline long __syscall2(long n, long a, long b)
 	register long a7 __asm__("a7") = n;
 	register long a0 __asm__("a0") = a;
 	register long a1 __asm__("a1") = b;
-	__asm_syscall("r"(a7), "0"(a0), "r"(a1))
+	register long a6 __asm__("a6") = PSEUDOS_SEND;
+	__asm_syscall("r"(a7), "0"(a0), "r"(a1), "r"(a6))
+	a6 = PSEUDOS_RECV;
+	__asm_syscall("r"(a6))
+	return a0;
 }
 
 static inline long __syscall3(long n, long a, long b, long c)
@@ -36,7 +51,11 @@ static inline long __syscall3(long n, long a, long b, long c)
 	register long a0 __asm__("a0") = a;
 	register long a1 __asm__("a1") = b;
 	register long a2 __asm__("a2") = c;
-	__asm_syscall("r"(a7), "0"(a0), "r"(a1), "r"(a2))
+	register long a6 __asm__("a6") = PSEUDOS_SEND;
+	__asm_syscall("r"(a7), "0"(a0), "r"(a1), "r"(a2), "r"(a6))
+	a6 = PSEUDOS_RECV;
+	__asm_syscall("r"(a6))
+	return a0;
 }
 
 static inline long __syscall4(long n, long a, long b, long c, long d)
@@ -46,7 +65,11 @@ static inline long __syscall4(long n, long a, long b, long c, long d)
 	register long a1 __asm__("a1") = b;
 	register long a2 __asm__("a2") = c;
 	register long a3 __asm__("a3") = d;
-	__asm_syscall("r"(a7), "0"(a0), "r"(a1), "r"(a2), "r"(a3))
+	register long a6 __asm__("a6") = PSEUDOS_SEND;
+	__asm_syscall("r"(a7), "0"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a6))
+	a6 = PSEUDOS_RECV;
+	__asm_syscall("r"(a6))
+	return a0;
 }
 
 static inline long __syscall5(long n, long a, long b, long c, long d, long e)
@@ -57,7 +80,11 @@ static inline long __syscall5(long n, long a, long b, long c, long d, long e)
 	register long a2 __asm__("a2") = c;
 	register long a3 __asm__("a3") = d;
 	register long a4 __asm__("a4") = e;
-	__asm_syscall("r"(a7), "0"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4))
+	register long a6 __asm__("a6") = PSEUDOS_SEND;
+	__asm_syscall("r"(a7), "0"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a6))
+	a6 = PSEUDOS_RECV;
+	__asm_syscall("r"(a6))
+	return a0;
 }
 
 static inline long __syscall6(long n, long a, long b, long c, long d, long e, long f)
@@ -69,7 +96,11 @@ static inline long __syscall6(long n, long a, long b, long c, long d, long e, lo
 	register long a3 __asm__("a3") = d;
 	register long a4 __asm__("a4") = e;
 	register long a5 __asm__("a5") = f;
-	__asm_syscall("r"(a7), "0"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5))
+	register long a6 __asm__("a6") = PSEUDOS_SEND;
+	__asm_syscall("r"(a7), "0"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(a6))
+	a6 = PSEUDOS_RECV;
+	__asm_syscall("r"(a6))
+	return a0;
 }
 
 #define VDSO_USEFUL
